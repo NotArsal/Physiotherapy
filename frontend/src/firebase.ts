1ef1c134059d0539d3e48af8267b69f0465dfc11
NotArsal <arsalanshaikh5123@gmail.com>
2026-04-28 20:57:33 +0530

    first commit

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOXfA_bo7nMCRsDMhen5wzTlXGivEKbHg",
+  authDomain: "physio-ml-7c08b.firebaseapp.com",
+  projectId: "physio-ml-7c08b",
+  storageBucket: "physio-ml-7c08b.firebasestorage.app",
+  messagingSenderId: "980988444841",
+  appId: "1:980988444841:web:0556e2863fdb2b53aee9b8",
+  measurementId: "G-G5EJT46RN4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export { analytics };
+export default app;
